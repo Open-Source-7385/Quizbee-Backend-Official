55778163e47a27f9093ecf91119de4264fb8ca74
DiegoLlamozas <diegollamozas911@gmail.com>
2025-10-29 15:41:06 -0500

    chore: setup project and add initial dependencies

---
+++ src/main/java/dominio/startup/quizbee/quiz_bee_platform/QuizBeePlatformApplication.java
@@ -0,0 +1,13 @@
+package dominio.startup.quizbee.quiz_bee_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBeePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBeePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dominio/startup/quizbee/quiz_bee_platform/QuizBeePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dominio.startup.quizbee.quiz_bee_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBeePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
